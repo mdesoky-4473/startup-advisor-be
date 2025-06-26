bd8fd6a0e0e56a6ba04f4a3c78ffa11fd8c5a889
Mohamed Desoky <mae.desoky@gmail.com>
2025-06-26 14:59:55 -0400

    Enable SSL for Render Postgres connection

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const isProd = process.env.NODE_ENV === "production";
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProd ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false, // Render requires this for SSL
+  },
 });
 
 client.connect();
+
 module.exports = client;
